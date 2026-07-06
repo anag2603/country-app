185aa405da7d0a29bd63d9be6af956c205a56cd5
ValeriaRojo <rh202354373@alm.buap.mx>
2026-07-06 12:23:50 -0600

    Configuración de PWA

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -13,6 +13,9 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withFetch()), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
           }),
   ]
 };
